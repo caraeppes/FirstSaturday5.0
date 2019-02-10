2eedd8a3758a59a373fc72586b6845e69c76a232
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-10 12:41:21 -0500

    completed first few for loop methods

--- WriteLoops.java
+++ WriteLoops.java
@@ -17,56 +17,47 @@ public class WriteLoops {
 
     public int oneToFive() {
         int w = 0;
-
-        // Write a FOR loop that counts from 1 to 10.
-            // calling
+        for (int i = 0; i < 5; i++){
             w = w + 1;
-            // each time through the loop
-
-        // this will tell the test how many times the loop executed.
+        }
         return w;
     }
 
     public int oneToTen() {
         int w = 0;
-
-        // Write a FOR loop that counts from 1 to 10.
-        // calling
-        w = w + 1;
-        // each time through the loop
-        
+        for (int i = 0; i < 10; i++){
+            w = w + 1;
+        }
         return w;
     }
 
     public int startAtTwentyOne() {
         int w = 0;
 
-        // Write a FOR loop that makes 10 iterations, start at 21.
-        // calling
-        w = w + 1;
-        // each time through the loop
+        for (int i = 21; i < 32; i++){
+            w = w + 1;
+        }
         
         return w;
     }
 
     public int countDown() {
         int w = 0;
-
-        // Write a FOR loop that counts down from 100 to 0.
-        // calling
-        w = w + 1;
-        // each time through the loop
         
-        return w;
+        for (int i = 100; i > 0; i --){
+            w = w + 1;
+        }
+
+       return w;
     }
 
     public int byTwoTo32() {
         int w = 0;
+        
+        for (int i = 0; i < 32; i += 2){
+            w = w + 1;
+        }
 
-        // Write a FOR loop from 0 to 32 by 2s.
-        // calling
-        w = w + 1;
-        // each time through the loop
         return w;
     }
 
